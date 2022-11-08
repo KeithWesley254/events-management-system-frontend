a1f2f0d941e243d40fa80d64914ad63f48445ecf
Keith <keithwesley187@gmail.com>
2022-11-08 12:05:01 +0300

    milestone: teesting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import LoginForm from './components/LoginForm';
+import Hero from './pages/Hero';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="overallTop">
+        <main>
+          <Routes>
+            <Route exact='true' path='/' element={<Hero />}/>
+            <Route exact='true' path='/login' element={<LoginForm />}/>
+            <Route exact='true' path='/homepage' element={<Hero />}/>
+          </Routes>
+        </main>
     </div>
   );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,118 @@
+import { Alert, Box, FormControl, FormHelperText, TextField } from '@mui/material';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [userData, setUserData] = useState({});
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    fetch("http://localhost:3000/users/sign_in", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ 
+        user: {
+            email, 
+            password 
+        }
+    }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => {
+          setUserData(user)
+          navigate('/homepage')
+        });
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+
+  console.log(userData)
+
+  return (
+    <>
+      <Box>
+        <main>
+        <form onSubmit = {handleSubmit}>
+          <p style={{fontWeight: "bolder", fontSize: 60}}>Login</p>
+          <p style={{fontWeight: "bold", color: "#545563", fontSize: 14}}>Sign in with the data you entered during your registration</p>
+          <Box sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}}>
+          
+            <div>
+              
+              <FormControl>
+                <TextField 
+                type="email"
+                variant="outlined"
+                label="Email"
+                id="email"
+                autoComplete="on"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} 
+                />
+                <FormHelperText id="my-helper-text">name@example.com</FormHelperText>
+              </FormControl>
+              <br />
+              <FormControl>
+                <TextField 
+                type="password"
+                label="Password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                <FormHelperText id="my-helper-text">min. 8 characters</FormHelperText>
+              </FormControl>
+            </div>
+          </Box>
+          
+          <br />
+          <br />
+          <div>
+            <FormControl>
+              <button style={{
+                fontSize: 14,
+                backgroundColor: "#4e60ff",
+                width: 255,
+                height: 40,
+                color: "#fff",
+                borderRadius: 10,
+                cursor: "pointer",
+                border: "none"
+              }}
+              type="submit"
+              >
+                {isLoading ? "Loading..." : "Login"}
+              </button>
+              <br />
+            </FormControl>  
+            <div>
+              {errors.map((err) => (
+              <>
+                <Alert key={err} severity="error" sx={{ width: '100%' }}>
+                  {err}
+                </Alert>
+              </>
+              ))}
+            </div>       
+          </div>
+          </form>
+        </main>
+      </Box>
+    </>
+  )
+}
+
+export default LoginForm;
