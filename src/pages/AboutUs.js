37033dcf85f4676278b48199f33b1932ecacf5d2
Keith <keithwesley187@gmail.com>
2022-11-09 20:58:40 +0300

    milestone: about page done

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,140 @@
-import React from 'react'
+import { Grid, Box, Card, CardMedia, Typography, Button } from '@mui/material';
+import React, { useEffect, useState } from 'react'
 
 const AboutUs = () => {
+  const [communityData, setCommunityData] = useState([]);
+  const [aboutUsData, setAboutUsData] = useState([]);
+  const [randomIndexCommunity, setRandomIndex] = useState();
+  const [randomIndexAbout, setRandomIndexAbout] = useState();
+
+  useEffect(() => {
+    changeCommunityImage()
+  }, []);
+
+  useEffect(() => {
+    changeAboutImage()
+  }, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/communities`)
+    .then(r => r.json())
+    .then(data => setCommunityData(data))
+  }, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/abouts`)
+    .then(r => r.json())
+    .then(data => setAboutUsData(data))
+  }, []);
+
+  function changeAboutImage(){
+    const randomNumber = Math.floor(Math.random() * aboutUsData.length);
+    setRandomIndexAbout(randomNumber)
+  }
+
+  function changeCommunityImage(){
+    const randomNumber = Math.floor(Math.random() * communityData.length);
+    setRandomIndex(randomNumber)
+  }
+
   return (
-    <div>AboutUs</div>
+    <>
+      <Box>
+      <br />
+        <Grid container spacing={2} columns={12}>
+          <Grid item xs={12} md={6}>
+            <div style={{
+              display: "flex", justifyContent: "center", alignItems: "center", fontSize: 30, fontFamily: "nunito", fontWeight: "bolder"
+              }}>
+                <p>
+                  About Us
+                </p>
+            </div>
+          </Grid>
+        </Grid>
+        <br />
+        <Grid container spacing={2} columns={12}>
+          <Grid item xs={12} md={6}>
+            <Box sx={{ml: 4, mr: 4}}>
+              <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <Card sx={{ maxWidth: 400 }}>
+                  <CardMedia
+                  component="img"
+                  height="350"
+                  image= {aboutUsData[randomIndexAbout]?.about_img}
+                  alt= "about-content"
+                  loading="lazy"
+                  />
+                </Card>
+              </div>
+            </Box> 
+          </Grid>
+          <br />
+          <Grid item xs={12} md={6}>
+            <Box sx={{ml: 4, mr: 4}}>
+              <div>
+                <Box>
+                  <Typography>
+                    {aboutUsData[randomIndexAbout]?.about_description}
+                  </Typography>
+                </Box>
+                <br />  
+                <Box sx={{display: "flex", justifyContent: "center"}}>
+                    <Button onClick={changeAboutImage} variant="contained">See More</Button>
+                </Box>
+              </div>
+            </Box> 
+          </Grid>
+        </Grid>
+        <br />
+        <Grid container spacing={2} columns={12}>
+          <Grid item xs={12} md={6}>
+            <div style={{
+              display: "flex", justifyContent: "center", alignItems: "center", fontSize: 30, fontFamily: "nunito", fontWeight: "bolder"
+              }}>
+                <p>
+                  Community Impact
+                </p>
+            </div>
+          </Grid>
+        </Grid>
+        <br />
+        <Grid container spacing={2} columns={12}>
+          <Grid item xs={12} md={6}>
+            <Box sx={{ml: 4, mr: 4}}>
+              <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <Card sx={{ maxWidth: 400 }}>
+                  <CardMedia
+                  component="img"
+                  height="350"
+                  image= {communityData[randomIndexCommunity]?.community_img}
+                  alt= "community-content"
+                  loading="lazy"
+                  />
+                </Card>
+              </div>
+            </Box> 
+          </Grid>
+          <br />
+          <Grid item xs={12} md={6}>
+            <Box sx={{ml: 4, mr: 4}}>
+              <div>
+                <Box>
+                  <Typography>
+                    {communityData[randomIndexCommunity]?.community_description}
+                  </Typography>
+                </Box>
+                <br />  
+                <Box sx={{display: "flex", justifyContent: "center"}}>
+                    <Button onClick={changeCommunityImage} variant="contained">See More</Button>
+                </Box>  
+              </div>
+            </Box> 
+          </Grid>
+        </Grid>
+        <br />
+      </Box>
+    </>
   )
 }
 
