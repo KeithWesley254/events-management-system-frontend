08020ed9be6c36a1feb649b7f7afa5bf7fd22de1
Keith <keithwesley187@gmail.com>
2022-11-10 11:41:56 +0300

    milestone: ready up

--- src/pages/SpecificCategory.js
+++ src/pages/SpecificCategory.js
@@ -22,9 +22,11 @@ const SpecificCategory = () => {
   if(isLoading === true) return (
     <Grid container spacing={2} columns={12}>
       <Grid xs={12} md={12}>
-        <div style={{marginTop: "25%"}}>
-          <div className="loader"></div>
-        </div>
+        <Box sx={{ borderRadius: 20, display: "inline-flex", position: "relative", width: "100%", justifyContent: "center", flexDirection: 'row' }}>
+          <div style={{marginTop: "25%", display: "inline-flex", justifyContent: "center"}}>
+            <div className="loader"></div>
+          </div>
+        </Box>
       </Grid>
     </Grid>
   )
